8ab3e99e9a3fbd5f44a5003c5b62c93f8b7d9e27
Mateo Carballo Alonso <a22mateoca@ateis.local>
2023-03-14 21:04:46 +0100

    .

---
+++ src/EmpleadoFijoTemporal/EmpFijo.java
@@ -0,0 +1,27 @@
+package EmpleadoFijoTemporal;
+
+import java.util.ArrayList;
+
+public class EmpFijo extends Empleado{
+
+    private String fechaInicio;
+    private String fechaFin;
+    private double precioDia;
+
+    private ArrayList<Ventas> listadoVentas = new ArrayList<>();
+
+    public EmpFijo(String nss, String nombre, String fechaDeNacimiento,
+                   char sexo, String fechaInicio, String fechaFin, double precioDia,
+                   ArrayList<Ventas> listadoVentas) {
+        super(nss, nombre, fechaDeNacimiento, sexo);
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.precioDia = precioDia;
+        this.listadoVentas = listadoVentas;
+    }
+
+    @Override
+    public float sueldo() {
+        return 0;
+    }
+}
